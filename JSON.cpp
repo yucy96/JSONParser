d382574b1d78c129ed97ca3c951cecd4c1b05426
Yvette W <yvette.wang2015@gmail.com>
2020-04-19 13:00:21 -0400

    Update JSON.cpp

--- JSON.cpp
+++ JSON.cpp
@@ -1,4 +1,4 @@
-// JSON.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// JSON.cpp : This file contains the 'main' function. Program execution begins and ends there
 //
 
 #include <iostream>
